02a27687d77956e03812ba7023583b50c1c3700c
wangtao <tao.wang@nuaa.edu.cn>
2023-04-21 18:05:04 +0800

    2023.04.21

---
+++ src/ListNode/GetIntersectionNode.java
@@ -0,0 +1,44 @@
+package ListNode;
+
+/*
+* 题目：给你两个单链表的头节点 headA 和 headB ，请你找出并返回两个单链表相交的起始节点。如果两个链表没有交点，返回 null 。
+* 时间复杂度：O(m + n)
+* 空间复杂度：O(1)
+*/
+public class GetIntersectionNode {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB){
+        ListNode curA = headA;
+        ListNode curB = headB;
+        int lenA = 0, lenB = 0;
+        while (headA != null){
+            headA = headA.next;
+            lenA++;
+        }
+        while (headB != null){
+            headB = headB.next;
+            lenB++;
+        }
+        curA = headA;
+        curB = headB;
+        if (lenA > lenB){
+            int temLen = lenA;
+            lenA = lenB;
+            lenB = temLen;
+            ListNode temNode = curA;
+            curA = curB;
+            curB = temNode;
+        }
+        int len = lenB - lenA;
+        while (len-- > 0){
+            curB = curB.next;
+        }
+        while (curB != null){
+            if (curB == curA){
+                return curB;
+            }
+            curB = curB.next;
+            curA = curA.next;
+        }
+        return null;
+    }
+}
---
+++ src/ListNode/RemoveNthFromEnd.java
@@ -0,0 +1,28 @@
+package ListNode;
+
+/*
+* 题目：给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。
+* 示例： 1 -> 2 -> 3 -> 4 -> null  (删除倒数第2个节点)
+*       1 -> 2 -> 4 -> null
+* 方法：使用两个指针 slow 和 fast，先让 fast 移动 n 步，再让两个指针同时移动，判断 fast.next 是否为空，为空时，此时 slow 指向被删除节点的前一节点。
+* 时间复杂度：O(n)
+* 空间复杂度：O(1)
+*/
+public class RemoveNthFromEnd {
+    public ListNode removeNthFromEnd(ListNode head, int n){
+        ListNode dummyNode = new ListNode(0);
+        dummyNode.next = head;
+        ListNode slowNode = dummyNode;
+        ListNode fastNode = dummyNode;
+//        while (n-- >0)
+        for (int i = 0; i < n; i++){
+            fastNode = fastNode.next;
+        }
+        while (fastNode.next != null){
+            fastNode = fastNode.next;
+            slowNode = slowNode.next;
+        }
+        slowNode.next = slowNode.next.next;
+        return dummyNode.next;
+    }
+}

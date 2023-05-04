d0ededf45080f201b85a05965c4d52b1d56381ab
wangtao <tao.wang@nuaa.edu.cn>
2023-05-04 20:54:32 +0800

    2023.05.04

---
+++ src/String/ReplaceSpace.java
@@ -0,0 +1,68 @@
+ package String;
+
+/*
+* 题目：请实现一个函数，把字符串 s 中的每个空格替换成"%20"。
+*      示例 1： 输入：s = "We are happy."
+*      输出："We%20are%20happy."
+* 方法：1、双指针法，首先将字符串长度扩充到目标长度(遍历到空格，长度加2)，设立两个指针，尾指针指向新长度下的末尾，头指针指向原长度下的末尾，从后往前遍历，遇到空格则将%20添加到字符串中；
+*      2、使用一个新的对象复制String，复制的过程中对其判断，是空格则替换，否则直接复制
+*/
+public class ReplaceSpace {
+    // 双指针法
+    // 时间复杂度：O(n)
+    // 空间复杂度：O(1)
+    public String replacespace1(String s){
+        if (s == null || s.length() == 0){
+            return s;
+        }
+        // 扩充字符串长度
+        StringBuilder str = new StringBuilder();
+        for (int i = 0; i < s.length(); i++){
+            if (s.charAt(i) == ' '){
+                str.append("  ");
+            }
+        }
+        // 判断是否有空格，没有则直接返回
+        if (str.length() == 0){
+            return s;
+        }
+        // 存在空格情况，定义两个指针
+        int left = s.length()-1;
+        s = s + str.toString();
+        int right = s.length()-1;
+        char[] result = s.toCharArray();
+        while (left >= 0){
+            if (result[left] == ' '){
+                result[right--] = '0';
+                result[right--] = '2';
+                result[right] = '%';
+            }else {
+                result[right] = result[left];
+            }
+            right--;
+            left--;
+        }
+        return new String(result);
+    }
+
+    // 定义一个新的对象复制原String
+    public String replaceSpace2(String s){
+        if (s == null || s.length() == 0)
+            return s;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++){
+            if (s.charAt(i) == ' '){
+                sb.append("%20");
+            }else {
+                sb.append(s.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+
+    // 调用内置函数
+    public String replaceSpace3(String s){
+        String rs = s.replace(" ", "%20");
+        return rs;
+    }
+}
---
+++ src/String/ReverseWords.java
@@ -0,0 +1,66 @@
+package String;
+
+/*
+* 题目：给你一个字符串 s ，请你反转字符串中 单词 的顺序。
+*      单词 是由非空格字符组成的字符串。s 中使用至少一个空格将字符串中的 单词 分隔开。
+*      返回 单词 顺序颠倒且 单词 之间用单个空格连接的结果字符串。
+*      注意：输入字符串 s 中可能会存在前导空格、尾随空格或者单词间的多个空格。返回的结果字符串中，单词间应当仅用单个空格分隔，且不包含任何额外的空格。
+* 方法：分为三个步骤：
+*      1、移除字符串中的多余空格 -- 使用双指针法
+*      2、翻转整个字符串
+*      3、将每个单词逆序
+* 时间复杂度：O(n)
+* 空间复杂度：O(1)
+*/
+public class ReverseWords {
+    // 移除多余空格
+    public StringBuilder removeExtraSpace(String s){
+        int start = 0;
+        int end = s.length()-1;
+        while (s.charAt(start) == ' ') start++;
+        while (s.charAt(end) == ' ') end--;
+        StringBuilder sb = new StringBuilder();
+        while (start <= end){
+            char ch = s.charAt(start);
+            if (ch != ' ' || sb.charAt(sb.length()-1) != ' '){
+                sb.append(ch);
+            }
+            start++;
+        }
+        return sb;
+    }
+
+    // 反转整个字符串
+    private void reverseString(StringBuilder sb, int start, int end){
+        while (start < end){
+            char temp = sb.charAt(start);
+            sb.setCharAt(start, sb.charAt(end));
+            sb.setCharAt(end, temp);
+            start++;
+            end--;
+        }
+    }
+
+    // 反转每个单词
+    private void reverseEachWord(StringBuilder sb){
+        int start = 0;
+        int end = 1;
+        int len = sb.length();
+        while (start < len){
+            while (end < len && sb.charAt(end) != ' '){
+                end++;
+            }
+            reverseString(sb, start, end-1);
+            start = end+1;
+            end = start+1;
+        }
+    }
+
+    public String reverseWords(String s){
+        StringBuilder sb = removeExtraSpace(s);
+        reverseString(sb, 0, sb.length()-1);
+        reverseEachWord(sb);
+
+        return sb.toString();
+    }
+}
